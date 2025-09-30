aa2b8dccc97fdb43ac0a589b2346d13fc90e2605
User <User@DESKTOP-40TE8DR>
2025-09-30 13:20:33 +1000

    изменение вёрстки и добавление класса

---
+++ ToDoList/Task.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateOnly DateTask { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
